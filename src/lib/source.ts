6670a59d9ec4af49c7f9d1a07363446fe17d7c08
YassirJr <yassir.junior.03@gmail.com>
2025-07-29 17:20:47 +0100

    feat: update source library functionality
    - Enhance source.ts with improved functionality
    - Update core library utilities

--- src/lib/source.ts
+++ src/lib/source.ts
@@ -1,7 +1,18 @@
-import { docs } from '../../.source';
-import { loader } from 'fumadocs-core/source';
+// @ts-ignore
+import { docs } from "../../.source";
+import { loader } from "fumadocs-core/source";
+import { icons } from "lucide-react";
+import { createElement } from "react";
 
 export const source = loader({
-    baseUrl: '/docs',
+    baseUrl: "/docs",
     source: docs.toFumadocsSource(),
+    icon(icon) {
+        if (!icon) {
+            // You may set a default icon
+            return;
+        }
+
+        if (icon in icons) return createElement(icons[icon as keyof typeof icons]);
+    },
 });
